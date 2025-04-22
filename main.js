0e84fafa118490488eac887b8310d2ff425711e3
TheRepeater77 <russchoolacc@gmail.com>
2025-04-22 11:39:13 +0800

    Added Accuracy and Quality of Life

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const gameArea = document.getElementById('gameArea');
 const target = document.getElementById('target');
 const scoreBoard = document.getElementById('scoreBoard');
+const Accuracy = document.getElementById('Accuracy');
+
+
 
 let score = 0;
+let missclick = 0;
+let accuracy = 0;
 
 function moveTarget() {
   const gameAreaRect = gameArea.getBoundingClientRect();
   const maxX = gameAreaRect.width - target.offsetWidth;
   const maxY = gameAreaRect.height - target.offsetHeight;
-
+  
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
-
+  
   target.style.left = `${randomX}px`;
   target.style.top = `${randomY}px`;
 }
@@ -20,8 +25,15 @@ function moveTarget() {
 // Initial target position
 moveTarget();
 
-const ball_listener = target.addEventListener("click",()=>{
+const target_click = target.addEventListener("contextmenu",(e)=>{
   moveTarget();
   score++;
   scoreBoard.innerHTML = "Score: " + score;
+});
+
+const disable_rmb_gameArea = gameArea.addEventListener("contextmenu", (e)=>{
+  e.preventDefault();
+  missclick++;
+  accuracy = Math.round(score/missclick * 100)/100;
+  Accuracy.innerHTML = "Accuracy: " + accuracy + "%";
 });
