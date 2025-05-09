c54cb081643a19ab638809771a0c6a74e8a3d257
TheRepeater77 <124745527+TheRepeater77@users.noreply.github.com>
2025-05-09 18:30:58 +0800

    Fully Added Scatter Mode

--- main.js
+++ main.js
@@ -11,42 +11,111 @@ const scatterHTMLForm = document.getElementById('scatter-form');
 const scatterFormInput = document.getElementById('ball_num_input');
 const scatterFormBtn = document.getElementById('scatter-generate-btn');
 
-let scatterBalls;
+let scatterBalls = [];
 
 let mode = 0;
 let score = 0;
 let total_clicks = 0;
 let accuracy = 0;
+let scatter_count = 0;
+let scatter_order = 0;
+let ball_num = 0;
 
-function moveTarget() {
+function moveTarget(input) {
   const gameAreaRect = gameArea.getBoundingClientRect();
-  const maxX = gameAreaRect.width - target.offsetWidth;
-  const maxY = gameAreaRect.height - target.offsetHeight;
+  const maxX = gameAreaRect.width - input.offsetWidth;
+  const maxY = gameAreaRect.height - input.offsetHeight;
   
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
   
-  target.style.left = `${randomX}px`;
-  target.style.top = `${randomY}px`;
+  input.style.left = `${randomX}px`;
+  input.style.top = `${randomY}px`;
 }
 
 function reset(){
   score = 0;
   total_clicks = 0;
   accuracy = 0;
+  scatter_count = 0;
+  scatter_order = 0;
+
+  moveTarget(target);
+  rescatter();
+
   scoreBoard.innerHTML = "Score: " + score;
   Accuracy.innerHTML = "Accuracy: " + accuracy + "%";
   TotalClicks.innerHTML = "Total Clicks: " + total_clicks;
 }
 
+function rescatter(){
+  scatterBalls.forEach(scatterBall => {
+    moveTarget(scatterBall);
+    if(scatterBall.classList.contains("hide")){
+      scatterBall.classList.toggle("hide");
+    }
+  });
+}
+
+function removeScatter(){
+  scatterBalls.forEach(scatterBall => {
+    scatterBall.remove();
+});
+}
+
+function increaseScore(){
+  score++;
+  scoreBoard.innerHTML = "Score: " + score;
+}
+
+function increaseClicks(){
+  total_clicks++;
+  TotalClicks.innerHTML = "Total Clicks: " + total_clicks;
+}
+
+function increaseAccuracy(){
+  if(mode == 0){
+    accuracy = Math.round(score/total_clicks * 100);
+  } else if (mode == 1 && scatter_count == 0){
+    accuracy = Math.round(score/(total_clicks/ball_num) * 100);
+  }
+  Accuracy.innerHTML = "Accuracy: " + accuracy + "%";
+}
 
 function generateScatterShot(){
-  let ball_num = parseInt(scatterFormInput.value)
+  ball_num = parseInt(scatterFormInput.value);
   for (let i = 0; i < ball_num; i++) {
     let scatter_ball = document.createElement("div");
+    let scatter_text = document.createElement("p");
     scatter_ball.classList = "scatter-targets ball";
-    scatter_ball.innerHTML = i;
+    scatter_text.classList = "scatter-text";
+    scatter_text.innerHTML = i + 1;
     gameArea.append(scatter_ball);
+    scatter_ball.append(scatter_text);
+    moveTarget(scatter_ball);
+    
+    const scatter_ball_click = scatter_ball.addEventListener("contextmenu",(e)=>{
+      e.preventDefault();
+      
+      if(scatter_count + 1 == ball_num){
+        scatter_count = 0;
+        scatter_order = 0;
+        increaseScore();
+        rescatter();
+      } else if (scatter_count < ball_num && scatter_order + 1 == scatter_text.innerHTML){
+        scatter_order++;
+        scatter_count++;
+        scatter_ball.classList.toggle("hide");
+      } else {
+        rescatter();
+        scatter_order = 0;
+        scatter_count = 0;
+        increaseAccuracy();
+      }
+    });
+
+    
+    scatterBalls.push(scatter_ball);
   }
 }
 
@@ -65,7 +134,11 @@ const scatterFormDisable = scatterHTMLForm.addEventListener("submit", (e)=>{
 
 // Scatter Generate Button
 const scatterGenerate = scatterFormBtn.addEventListener("click",()=>{
-  generateScatterShot();
+  if (parseInt(scatterFormInput.value) <= 10){
+    reset();
+    removeScatter();
+    generateScatterShot();
+  }
 });
 
 // Change Modes
@@ -75,6 +148,8 @@ const moveTargetClick = movingTargetBtn.addEventListener("click",()=>{
     reset();
     movingTargetTitle.classList.toggle("hide");
     scatterShotTitle.classList.toggle("hide");
+    removeScatter();
+    target.classList.toggle("hide");
   }
 });
 const scatterShotClick = scatterShotBtn.addEventListener("click",()=>{
@@ -83,27 +158,24 @@ const scatterShotClick = scatterShotBtn.addEventListener("click",()=>{
     reset();
     movingTargetTitle.classList.toggle("hide");
     scatterShotTitle.classList.toggle("hide");
+
+    target.classList.toggle("hide");
   }
 });
 
 
 // Initial target position
-moveTarget();
+moveTarget(target);
 
 const target_click = target.addEventListener("contextmenu",(e)=>{
   if(mode == 0){
-    moveTarget();
-    score++;
-    scoreBoard.innerHTML = "Score: " + score;
+    moveTarget(target);
+    increaseScore();
   }
 });
 
 const disable_rmb_gameArea = gameArea.addEventListener("contextmenu", (e)=>{
   e.preventDefault();
-  if(mode == 0){
-    total_clicks++;
-    accuracy = Math.round(score/total_clicks * 100);
-    Accuracy.innerHTML = "Accuracy: " + accuracy + "%";
-    TotalClicks.innerHTML = "Total Clicks: " + total_clicks;
-  }
+  increaseClicks();
+  increaseAccuracy();
 });
